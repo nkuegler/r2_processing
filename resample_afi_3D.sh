8f5e8309d8a77703e5d2bfff77c751ae867e4cac
Niklas Kuegler <kuegler@cbs.mpg.de>
2025-04-16 15:58:20 +0200

    *Adjust main script to run through completely. Add scripts to run resampling of AFI maps
    Make sure to checkout the the commit from Feb 21 for the main script to work. There are two bash scripts that perform the resampling, one for 3D and one for 4D data

---
+++ resample_afi_3D.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+parent_dir=$1
+input_fn=$2
+reference_fn=$3
+output_fn=$4
+int_mode=$5
+
+
+# Good scientists abort computations upon unexpected problems
+set -e
+
+## The more scientific way with increased reproducibility:
+fsl="sc fsl 6.0.6"
+ants="sc ants 2.5.4"
+
+cd $parent_dir
+echo "Cropping reference image to only two echoes"
+$fsl fslroi $reference_fn ref_tmp.nii.gz 0 -1 0 -1 0 -1 0 1    # cut the reference image to only two echoes
+echo "Resampling input image to reference image space"
+$ants antsApplyTransforms -d 3 -i $input_fn -r ref_tmp.nii.gz -o $output_fn -n $int_mode
+echo "Resampling done"
+echo "Cleaning up"
+rm ref_tmp.nii.gz
